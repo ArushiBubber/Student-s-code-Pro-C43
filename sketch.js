440b0e9bac79187152e68f99f4ce6e9b30186dc1
ArushiBubber <77745574+ArushiBubber@users.noreply.github.com>
2021-07-27 23:12:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
-var passedFinish;
+
 var obstacles;
 var s,i;
 var form, player, game;
@@ -22,9 +22,7 @@ f2 = loadImage("images/f1.png");
   car3_img = loadImage("images/car3.png");
   car4_img = loadImage("images/car4.png");
   ground = loadImage("images/ground.png");
-  bronze_img = loadImage("images/bronze.png");
-  silver_img = loadImage("images/silver.png");
-  gold_img = loadImage("images/gold.png");
+ 
 }
 
 function setup(){
@@ -32,7 +30,7 @@ function setup(){
   database = firebase.database();
   gameState = 0;
   distance = 0;
-  finishedPlayers = 0;
+  
   yVel = 0;
   xVel = 0;
   obstacles=createGroup();
@@ -60,9 +58,7 @@ function draw(){
    //start the game
  
   //start the game
-  if (playerCount === 4 && finishedPlayers === 0) {
-    game.update(1);
-  }
+ 
 
   //start the game for real
   if (gameState === 1) {
@@ -70,15 +66,12 @@ function draw(){
   }
 
   //end the game
-  if (finishedPlayers === 4) {
-    game.update(2);
+  
     //gameState = 2;
   }
 
   //display ranking
-  if (gameState === 2 && finishedPlayers === 4) {
-    game.displayRanks();
-  }
+ 
 }
 function keyPressed() {
   if (keyCode === 13 && gameState !== 1 && passedFinish === false) {
